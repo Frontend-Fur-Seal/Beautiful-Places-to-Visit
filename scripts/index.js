0218b71fdca057a54cd6a73fb178e6ae90b18836
VladimirSherstnev <voralbergann@gmail.com>
2023-01-30 04:04:44 +0300

    ref: переделка кода

--- scripts/index.js
+++ scripts/index.js
@@ -2,33 +2,39 @@
   let profileOccupation = document.querySelector(".profile__occupation");
   let buttonNameChange = document.querySelector(".profile__button-name-change");
   let buttonAddPlace = document.querySelector(".profile__button-add-place");
-  let popup = document.querySelector(".popup");
+
+  let popupChangeName = document.querySelector('.popup_name-change');
+  let popupAddPlace = document.querySelector('.popup_add-place');
   let form = document.querySelector(".popup__form");
   let popupName = form.querySelector(".popup__content_type_name");
   let popupOccupation = form.querySelector(".popup__content_type_occupation");
-  let closePopup = popup.querySelector(".popup__close");
+  
+  let popup = document.querySelector('.popup');
 
-function popupOpen(){
-  popup.classList.add("popup_opened");
-  popupName.value = profileName.textContent;
-  popupOccupation.value = profileOccupation.textContent;
+  let popupNamePlace = form.querySelector(".popup__content_type_name-place");
+  let popupLink = form.querySelector('.popup__content_type_link');
 
+  let closePopup = document.querySelector(".popup__close");
+
+
+function popupOpen(popupType){
+  popupType.classList.toggle('popup_opened');
+  
+  if(popupType.className.includes('name-change')){
+    popupName.value = profileName.textContent;
+    popupOccupation.value = profileOccupation.textContent;
+  }
 }
 
-function handleFormSubmit (evt) {
-    evt.preventDefault(); 
-    addPlace();
-    handleFormReset();
-}
 
-function handleFormReset(){
-    popup.classList.remove("popup_opened");
-}
+buttonNameChange.addEventListener('click', () => popupOpen(popupChangeName));
+buttonAddPlace.addEventListener('click', () => popupOpen(popupAddPlace));
 
-buttonNameChange.addEventListener("click", popupOpen);
-buttonAddPlace.addEventListener("click", popupOpen);
-form.addEventListener("submit", handleFormSubmit);
-closePopup.addEventListener("click", handleFormReset);
+
+
+
+
+
 
 const initialCards = [
   {
@@ -57,18 +63,44 @@ const initialCards = [
   }
 ]; 
 
-const elementTemplate = document.querySelector('#element-template').content;
+
+const elementTemplate = document.querySelector('#element-template').content.querySelector('.element');
 const elements = document.querySelector('.elements');
 
-initialCards.forEach(function(item){
+function templateCard(){
+    const cards = initialCards.map(function(item){
+    const card = elementTemplate.cloneNode(true);
+    const elementLike = document.querySelector(".element__like");
+    const elementDelete = document.querySelector('.element__delete');
+    card.querySelector('.element__name').textContent = item.name;
+    card.querySelector('.element__photo').src = item.link;
+    
+    return card
+ 
+  })
 
-const element = elementTemplate.querySelector('.element').cloneNode(true);
-element.querySelector('.element__photo').src = item.link;
-element.querySelector('.element__name').textContent = item.name;
-elements.append(element); 
+  elements.append(...cards);
+}
 
-})
+templateCard();
 
+
+/*
+function makePlace(name, link){
+
+
+  const element = elementTemplate.querySelector('.element').cloneNode(true);
+  element.querySelector('.element__name').textContent = name;
+  element.querySelector('.element__photo').src = link;
+  console.log(elementDelete);
+  return element
+}
+
+for(i = 0; i < initialCards.length; i++){
+  const cardItems = makePlace(initialCards[i].name, initialCards[i].link);
+  elements.prepend(cardItems);
+}
+/*
 
 function addPlace(){
 
@@ -85,6 +117,21 @@ function addPlace(){
     
 }
 
+/*
+
+const elementTemplate = document.querySelector('#element-template').content;
+const elements = document.querySelector('.elements');
+
+initialCards.forEach(function(item){
+
+const element = elementTemplate.querySelector('.element').cloneNode(true);
+element.querySelector('.element__photo').src = item.link;
+element.querySelector('.element__name').textContent = item.name;
+elements.append(element); 
+
+})
+
+*/
 
 
 
